7d58c7e94f33a9f6111f51d849f3f1ff45ba833f
thpssngr <thpssngr@gmail.com>
2017-11-16 21:05:56 +0100

    fixed missing releaseyear declaration

--- media.py
+++ media.py
@@ -6,7 +6,7 @@ class Movie():
     def __init__(self, movie_title, movie_releaseyear, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
-        self.releaseyear
+        self.releaseyear = movie_releaseyear
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
